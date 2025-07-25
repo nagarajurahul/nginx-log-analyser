c95d970e45c2e04da13ff75622f52d61700cd2e7
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-07-25 08:41:43 -0500

    add: filter IP count >30

--- analyse-nginx-logs.sh
+++ analyse-nginx-logs.sh
@@ -7,3 +7,6 @@
 # count unique occurrences, and sort them in reverse numerical order.
 # The output will show the number of requests per IP address.
 awk '{print $1}' logs.txt | sort | uniq --c | sort -rn
+
+# To filter out IP addresses with more than 30 requests,
+# awk '{print $1}' logs.txt | sort | uniq --c | sort -rn | awk '$1>30{print}'
